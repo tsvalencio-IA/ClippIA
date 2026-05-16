ccc64071b0404a9781fa7ad9aa3d53580b81eb66
tsvalencio-IA <tsvalencio@gmail.com>
2026-05-16 08:28:04 -0300

    Update Firebase configuration with new project details

--- firebase-config.js
+++ firebase-config.js
@@ -2,11 +2,11 @@
 // IMPORTANTE: agora usamos Realtime Database, então databaseURL é obrigatório.
 
 window.firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI.firebaseapp.com",
-  databaseURL: "https://COLE_AQUI-default-rtdb.firebaseio.com",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI.appspot.com",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
-};
+    apiKey: "AIzaSyCyxSNTLVc1LSYP0OdG0sc2bY8lBfqHo5Q",
+    authDomain: "clippia-c9910.firebaseapp.com",
+    databaseURL: "https://clippia-c9910-default-rtdb.firebaseio.com",
+    projectId: "clippia-c9910",
+    storageBucket: "clippia-c9910.firebasestorage.app",
+    messagingSenderId: "689937739065",
+    appId: "1:689937739065:web:c9a4b823a80245a4b50ce8"
+  };
